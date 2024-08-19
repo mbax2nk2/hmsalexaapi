f17f287829b0254b9c688769a297f9e39c2baf7b
Nursultan Kenenbayev <nkenenbayev@arlo.com>
2024-08-19 16:33:42 +0500

    first commit

---
+++ src/main/java/kz/nursultan/harness/HarnessApplication.java
@@ -0,0 +1,13 @@
+package kz.nursultan.harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/nursultan/harness/HarnessApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nursultan.harness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
